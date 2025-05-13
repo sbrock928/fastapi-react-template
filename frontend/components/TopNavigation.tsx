531e64e36a39711aca565438062898324b5a0d66
sbrock928 <97187437+sbrock928@users.noreply.github.com>
2025-05-13 17:52:51 -0400

    fixed broken logo

--- frontend/components/TopNavigation.tsx
+++ frontend/components/TopNavigation.tsx
@@ -1,9 +1,11 @@
+import viteLogo from '../assets/vite.svg';
+
 const TopNavigation = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark border-bottom top-navbar" style={{ backgroundColor: '#93186C' }}>
       <div className="container-fluid">
         <a className="navbar-brand d-flex align-items-center" href="/">
-          <img src="/vite.svg" alt="Vibez Logo" className="me-2" style={{ height: "24px" }} />
+          <img src={viteLogo} alt="Vibez Logo" className="me-2" style={{ height: "24px" }} />
         </a>
         <div className="ms-auto d-flex">
           <span className="navbar-text text-white">
